ddadcdc6e7dd8168d6582c3ee47ff859866539df
Anand <maanand2k@gmail.com>
2023-11-30 16:20:49 +0530

    Initial Commit

---
+++ src/test/java/com/codeforyou/springjetty/SpringJettyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeforyou.springjetty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringJettyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
